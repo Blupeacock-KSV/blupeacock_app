40d1a06c251943f77190bed0f6043c21626de294
NathiyaGopal <NathiyaGopal@users.noreply.github.com>
2026-06-10 06:03:21 +0000

    deploy: Blupeacock-KSV/Blupeacock_Frontend@ccc1d1d1a70c1c1ec6f65f82c678d91ca1dc02c2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1268736622" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3237134542" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
