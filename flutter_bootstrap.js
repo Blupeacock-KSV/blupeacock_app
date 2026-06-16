cadc316bff9d75646039d1fd7676771d7cc55ba1
hemanath6065 <hemanath6065@users.noreply.github.com>
2026-06-16 05:35:53 +0000

    deploy: Blupeacock-KSV/Blupeacock_Frontend@46d02eba3890a5284240f6276817d21fb651d4d9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "971523652" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3451799489" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
